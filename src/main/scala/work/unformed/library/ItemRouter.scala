0e785124808a9116ff7d1927f2bad1631cd88e13
gennady <gennady.lebedev@gmail.com>
2018-05-05 17:55:39 +0300

    make router simpler, add query support

--- src/main/scala/work/unformed/library/ItemRouter.scala
+++ src/main/scala/work/unformed/library/ItemRouter.scala
@@ -6,13 +6,12 @@ import akka.http.scaladsl.server.Route
 import work.unformed.library.AppContext.itemsRepository
 import work.unformed.library.model.Item
 import work.unformed.rest.JsonUtil
-import work.unformed.rest.meta.Query
-import work.unformed.rest.repository.{ConflictOnDelete, NothingToUpdate, RepositoryItemNotFound}
+import work.unformed.rest.meta.{Query, QuerySupport}
 import io.circe.generic.extras.auto._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import JsonUtil._
 
-object ItemRouter {
+object ItemRouter extends QuerySupport[Item] {
   lazy val routes: Route = metaRoute ~ collectionRoutes ~ itemRoutes
 
   private val metaRoute: Route = path("items" / "meta") {
@@ -23,37 +22,26 @@ object ItemRouter {
 
   private val collectionRoutes: Route = path("items") {
     get {
-      complete (StatusCodes.OK, itemsRepository.find(new Query[Item]))
+      resourceQuery { query =>
+        complete (StatusCodes.OK, itemsRepository.find(query))
+      }
     } ~ post {
-      entity(as[Item]) {
-        draft => complete(StatusCodes.Created, itemsRepository.create(draft))
+      entity(as[Item]) { draft =>
+        complete(StatusCodes.Created, itemsRepository.create(draft))
       }
     }
   }
 
-  private val itemRoutes: Route = path("item" / LongNumber) { id =>
+  private val itemRoutes: Route = path("items" / LongNumber) { id =>
     get {
-      itemsRepository.findById(id) match {
-        case Some(found) => complete(StatusCodes.OK, found)
-        case None => complete(StatusCodes.NotFound)
-      }
+      complete(StatusCodes.OK, itemsRepository.get(id))
     } ~ put {
       entity(as[Item]) { item =>
-        itemsRepository.findById(id) match {
-          case Some(existing) if existing != item =>
-            complete(StatusCodes.OK, itemsRepository.update(item))
-          case Some(existing) => throw new NothingToUpdate
-          case None => throw new RepositoryItemNotFound(id)
-        }
+        complete(StatusCodes.OK, itemsRepository.update(item))
       }
     } ~ delete {
       entity(as[Item]) { item =>
-        itemsRepository.findById(id) match {
-          case Some(existing) if existing == item =>
-            complete(StatusCodes.NoContent, itemsRepository.delete(item))
-          case Some(existing) => throw new ConflictOnDelete
-          case None => throw new RepositoryItemNotFound(id)
-        }
+        complete(StatusCodes.NoContent, itemsRepository.delete(item))
       }
     }
   }
