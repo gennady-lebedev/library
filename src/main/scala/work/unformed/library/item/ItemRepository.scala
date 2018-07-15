707dcec37918b8cba3001f3658646d1d889e0553
gennady <gennady.lebedev@gmail.com>
2018-07-15 13:16:00 +0300

    implement item repository (without audit logs)

--- src/main/scala/work/unformed/library/item/ItemRepository.scala
+++ src/main/scala/work/unformed/library/item/ItemRepository.scala
@@ -1,18 +1,35 @@
 package work.unformed.library.item
 
-import work.unformed.meta.{Query, Result}
+import scalikejdbc.{AutoSession, DBSession}
+import work.unformed.meta.{DBMapping, Defaults, Query, Result}
+import work.unformed.repository.{JdbcRepository, RepositoryItemNotFound}
 
-class ItemRepository {
-  def find(query: Query[ItemDB]): Result[ItemDB] = ???
+class ItemRepository(implicit val itemDB: DBMapping[ItemDB]) {
+  private val itemDbRepo = JdbcRepository[ItemDB]
 
-  def get(bookId: Long, itemId: Long): ItemDB = ???
+  def find(query: Query[ItemDB])(implicit session: DBSession = AutoSession): Result[ItemDB] =
+    itemDbRepo.find(query)
 
-  def create(bookId: Long, draft: ItemDraft): ItemDB = ???
+  def get(bookId: Long, itemId: Long)(implicit session: DBSession = AutoSession): ItemDB = {
+    val db = itemDbRepo.get(itemId)
+    if(db.bookId != bookId) throw new RepositoryItemNotFound(itemId, bookId)
+    else db
+  }
 
-  def modify(bookId: Long, item: ItemDraft): ItemDB = ???
+  def create(bookId: Long, draft: ItemDraft)(implicit session: DBSession = AutoSession): ItemDB =
+    itemDbRepo.create(ItemDB(
+      Defaults.long,
+      bookId,
+      Draft,
+      draft.holder,
+      draft.place,
+      draft.dueDate
+    ))
 
-  def remove(bookId: Long, itemId: Long): ItemDB = ???
-
-
+  def remove(bookId: Long, itemId: Long)(implicit session: DBSession = AutoSession): Unit = {
+    val db = itemDbRepo.get(itemId)
+    if(db.bookId != bookId) throw new RepositoryItemNotFound(itemId, bookId)
+    itemDbRepo.remove(db)
+  }
 
 }
